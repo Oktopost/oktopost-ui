aa2d3dc1e128cdd857c5f9b0da472349dab9770e
Ivan Demchenkov <ivan@oktopost.com>
2017-06-15 14:10:48 +0300

    test creating in progress

---
+++ test/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var ns = require('oktopost-namespace').dynamic(__dirname + '/..').OUI;
+
+console.log(ns);
+
+module.exports = require('oktopost-namespace').dynamic(__dirname + '/..');
---
+++ test/src/pos.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+const Namespace = require('../index');
+
+const Pos = Namespace.get('OUI.core.positioning.Pos');
+
+const assert = require('chai').assert;
+
+
+suite('pos library' ,function () 
+{
+
+});
