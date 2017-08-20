52d4ec0d68f5661ed45005052dbbbca739bb7400
Liad Guez <liad@oktopost.com>
2017-08-20 20:29:21 +0300

    Minor fix

--- dist/okto-ui.js
+++ dist/okto-ui.js
@@ -3606,7 +3606,7 @@ namespace('OUI.Components.List', function (window)
 		if (newTotal >= total)
 			return;
 
-		if ((page + 1) * count >= newTotal) 
+		if ((page + 1) * count > newTotal) 
 			return;
 
 		if (page === 0)
