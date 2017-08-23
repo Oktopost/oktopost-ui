23523735b1e5941d5563982889c4bf23e11f4459
Ivan Demchenkov <ivan@oktopost.com>
2017-08-23 11:53:00 +0300

    Build dist

--- dist/okto-ui.js
+++ dist/okto-ui.js
@@ -4303,18 +4303,23 @@ namespace('OUI.Views', function (window)
 		this._positionConfig	= positionConfig || {};
 
 		this._bindOpen();
-	};
+	}
 
 
 	MenuView.prototype._bindOpen = function ()
 	{
-		this._toggleElement.on('click', this._menu.open);
+		this._toggleElement.on('click.' + this._menu.getId(), this._menu.open);
+	};
+	
+	MenuView.prototype._unbindOpen = function () 
+	{
+		this._toggleElement.off('click.' + this._menu.getId());	
 	};
 
 
 	MenuView.prototype.bindRemove = function ()
 	{
-		this.getContainer().on('click', this._underlay, this._menu.close);
+		this.getContainer().on('click.' + this._menu.getId(), this._underlay, this._menu.close);
 	};
 
 	MenuView.prototype.getContainer = function ()
@@ -4325,6 +4330,7 @@ namespace('OUI.Views', function (window)
 	MenuView.prototype.remove = function ()
 	{
 		this.getContainer().remove();
+		this._unbindOpen();
 	};
 
 	MenuView.prototype.show = function ()
