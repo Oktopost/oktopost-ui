54d1e6731a055ffcc3d9f360811bcc238c1730a4
Ivan Demchenkov <ivan@oktopost.com>
2017-09-20 11:07:01 +0300

    Added Editable content component

---
+++ src/Components/EditableContent.js
@@ -0,0 +1,82 @@
+namespace('OUI.Components', function (window)
+{
+	var Event = window.Duct.Event;
+	var EditableContentView = window.OUI.Views.EditableContentView;
+
+	var classify = window.Classy.classify;
+
+
+	/**
+	 * @class OUI.Components.EditableContent
+	 */
+	function EditableContent(selector)
+	{
+		classify(this);
+		
+		this._view = new EditableContentView(selector);
+		
+		this._view.onContentChanged(this._contentChanged);
+		this._view.onEditActivate(this.enable);
+		this._view.onEditFinished(this.disable);
+		
+		this._onContentChanged = new Event('EditableContent.onContentChange');
+		
+		this._onBeforeEnable 	= new Event('EditableContent.onBeforeEnable');
+		this._onAfterEnable 	= new Event('EditableContent.onAfterEnable');
+		this._onBeforeDisable	= new Event('EditableContent.onBeforeDisable');
+		this._onAfterDisable	= new Event('EditableContent.onAfterDisabled');
+	}
+	
+	
+	EditableContent.prototype._contentChanged = function (element, content)
+	{
+		this._onContentChanged.trigger(element, content);	
+	};
+	
+	
+	EditableContent.prototype.onChange = function (callback)
+	{
+		this._onContentChanged.add(callback);
+	};
+	
+	EditableContent.prototype.onBeforeEnable = function (callback)
+	{
+		this._onBeforeEnable.add(callback);
+	};
+	
+	EditableContent.prototype.onAfterEnable = function (callback)
+	{
+		this._onAfterEnable.add(callback);
+	};
+	
+	EditableContent.prototype.onBeforeDisable = function (callback)
+	{
+		this._onBeforeDisable.add(callback);
+	};
+	
+	EditableContent.prototype.onAfterDisabled = function (callback)
+	{
+		this._onAfterDisable.add(callback);
+	};
+	
+	EditableContent.prototype.enable = function ()
+	{
+		this._onBeforeEnable.trigger(this._view.getElement());
+		
+		this._view.enable();
+		
+		this._onAfterEnable.trigger(this._view.getElement());
+	};
+	
+	EditableContent.prototype.disable = function ()
+	{
+		this._onBeforeDisable.trigger(this._view.getElement());
+		
+		this._view.disable();
+		
+		this._onAfterDisable.trigger(this._view.getElement());
+	};
+	
+	
+	this.EditableContent = EditableContent;
+});
---
+++ src/Views/EditableContentView.js
@@ -0,0 +1,123 @@
+namespace('OUI.Views', function (window)
+{
+	var is			= window.Plankton.is;
+	var Event 		= window.Duct.Event;
+	var classify 	= window.Classy.classify;
+
+
+	/**
+	 * @class OUI.Components.EditableContentView
+	 */
+	function EditableContentView(selector)
+	{
+		classify(this);
+
+		this._element = is.object(selector) ? selector : $(selector);
+
+		this._content = null;
+		this._onEditActivate = new Event('EditableContentView.onEditActivate');
+		this._onEditFinished = new Event('EditableContentView.onEditFinished');
+				
+		this._onContentChanged = new Event('EditableContentView.onContentChanged');
+		
+		this._bindEnableEvent();
+	}
+	
+	
+	EditableContentView.prototype._bindEnableEvent = function ()
+	{
+		var activateEdit = this._onEditActivate;
+		
+		this._element.on('dblclick', function (e)
+		{
+			activateEdit.trigger();
+			$(this).focus();
+		});
+	};
+	
+	EditableContentView.prototype._editFinished = function (el)
+	{
+		if (el.text() !== this._content)
+		{
+			this._onContentChanged.trigger(this._element, this._element.text());
+		}
+		
+		this._onEditFinished.trigger(this._element);
+	};
+	
+	EditableContentView.prototype._bindEvents = function ()
+	{
+		var editFinished = this._editFinished;
+		
+		this._element.on('blur', function (e)
+		{
+			editFinished($(this));
+		});
+		
+		this._element.on('keydown', function (e)
+		{
+			if (e.keyCode === 13)
+			{
+				$(this).blur();
+			}
+		});
+	};
+	
+	EditableContentView.prototype._unbindEvents = function ()
+	{
+		this._element.off('blur');
+		this._element.off('keydown');
+	};
+	
+	
+	EditableContentView.prototype.onEditActivate = function (callback)
+	{
+		this._onEditActivate.add(callback);
+	};
+	
+	EditableContentView.prototype.onEditFinished = function (callback)
+	{
+		this._onEditFinished.add(callback);
+	};
+	
+	EditableContentView.prototype.onContentChanged = function (callback)
+	{
+		this._onContentChanged.add(callback);
+	};
+	
+	EditableContentView.prototype.getElement = function ()
+	{
+		return this._element;	
+	};
+	
+	EditableContentView.prototype.enable = function ()
+	{
+		if (this._element.attr('contenteditable') === 'true')
+		{
+			return;
+		}
+		
+		this._content = this._element.text();
+		
+		this._element.attr('contenteditable', 'true');	
+		
+		this._bindEvents();
+	};
+	
+	EditableContentView.prototype.disable = function ()
+	{
+		if (this._element.attr('contenteditable') === 'false')
+		{
+			return;
+		}
+		
+		this._content = null;
+		
+		this._element.removeAttr('contenteditable');
+		
+		this._unbindEvents();
+	};
+	
+	
+	this.EditableContentView = EditableContentView;
+});
