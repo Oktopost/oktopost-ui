cc989acb1a8e30dd3f6b78623742005ff1ca21a0
Daniel Podrabinek <dpodrabinek@oktopost.com>
2019-04-16 14:09:47 +0300

    Added option to override Dialog view

--- dist/okto-ui.js
+++ dist/okto-ui.js
@@ -5882,8 +5882,9 @@ namespace('OUI.Components', function (window)
 
 	var classify 	= window.Classy.classify;
 	var idGenerator = window.OUI.Core.View.idGenerator;
+	var is			= window.Plankton.is;
 
-
+	
 	/**
 	 * @class OUI.Components.Dialog
 	 */
@@ -5891,24 +5892,46 @@ namespace('OUI.Components', function (window)
 	{
 		classify(this);
 
-		this._id 			= idGenerator('oui-dialog');
+		this._id 				= idGenerator('oui-dialog');
+		this._okButtonText		= okButtonText;
+		this._cancelButtonText	= cancelButtonText;
 		
-		this._view 			= new DialogView(this._id, okButtonText, cancelButtonText);
-
 		this._onCancel 		= new Event('dialog.onCancel');
 		this._onConfirm 	= new Event('dialog.onConfirm');
 		this._onOpen 		= new Event('dialog.onOpen');
-
-		this._view.onCancelClick(this._onCancel.trigger);
-		this._view.onConfirmClick(this._onConfirm.trigger);
+		
+		this._view = null;
 	}
-
-
+	
+	
+	Dialog.prototype._getView = function ()
+	{
+		if (!is(this._view))
+		{
+			this._view 	= new DialogView(this._id, this._okButtonText, this._cancelButtonText);
+			this._view.onCancelClick(this.triggerCancelEvent);
+			this._view.onConfirmClick(this.triggerConfirmEvent);
+		}
+		
+		return this._view;
+	};
+	
+	
 	Dialog.prototype.getId = function ()
 	{
 		return this._id;
 	};
-
+	
+	Dialog.prototype.triggerCancelEvent = function()
+	{
+		this._onCancel.trigger();
+	};
+	
+	Dialog.prototype.triggerConfirmEvent = function()
+	{
+		this._onConfirm.trigger();
+	};
+	
 	Dialog.prototype.onOpen = function (callback)
 	{
 		this._onOpen.add(callback);
@@ -5926,13 +5949,13 @@ namespace('OUI.Components', function (window)
 
 	Dialog.prototype.open = function (message) 
 	{
-		this._view.show(message);
+		this._getView().show(message);
 		this._onOpen.trigger(this._id);
 	};
 	
 	Dialog.prototype.close = function()
 	{
-		this._view.remove();
+		this._getView().remove();
 	};
 
 	Dialog.prototype.confirm = function () 
@@ -5943,6 +5966,11 @@ namespace('OUI.Components', function (window)
 	Dialog.prototype.cancel = function () 
 	{
 		this._onCancel.trigger(this._id);
+	};
+	
+	Dialog.prototype.setView = function (view)
+	{
+		this._view = view;
 	};
 
 
